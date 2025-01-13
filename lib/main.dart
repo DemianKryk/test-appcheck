204bdeb8a4728066860b93ddc15880d437c80cca
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-01-13 22:30:00 +0200

    text LayoutBuilder and size bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-import 'package:test_appcheck/firebase_options.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final app = await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // FirebaseStorage.instanceFor(app: app);
-  await FirebaseAppCheck.instanceFor(app: app).activate(
-    webProvider: ReCaptchaV3Provider(
-      '6LfKGjcqAAAAAIUz_yV0cTKcPA54o5VFXz64_3di',
-    ),
-  );
-
-  // await FirebaseAppCheck.instance.activate(
-  //   webProvider:
-  //       ReCaptchaV3Provider('6LfKGjcqAAAAAIUz_yV0cTKcPA54o5VFXz64_3di'),
-  //   androidProvider:
-  //       kReleaseMode ? AndroidProvider.playIntegrity : AndroidProvider.debug,
-  //   appleProvider:
-  //       kReleaseMode ? AppleProvider.deviceCheck : AppleProvider.debug,
-  // );
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -62,126 +24,110 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ScreenSizeBloc(),
+      child: Column(
+        children: [
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return TestWidget(
+                logText: 'LayoutBuilder REBUILD',
+                isDesk: constraints.maxWidth > 700,
+              );
+            },
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          BlocBuilder<ScreenSizeBloc, bool>(
+            builder: (context, state) {
+              return TestWidget(
+                logText: 'BLOC REBUILD',
+                isDesk: state,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var value = 'You have pushed the button this many times:';
-  String image =
-      'https://firebasestorage.googleapis.com/v0/b/test-appcheck-ef87e.appspot.com/o/story.png?alt=media&token=0e6fd1f2-f015-4c30-aa6f-228a20db9e78';
-  // late FirebaseStorage firebaseStorage;
-  late FirebaseFirestore firebaseFirestore;
-  @override
-  void initState() {
-    super.initState();
-    // firebaseStorage = FirebaseStorage.instance;
-    firebaseFirestore = FirebaseFirestore.instance;
-    _incrementCounter();
-  }
-
-  Future<void> _incrementCounter() async {
-    final text = await getTextTest();
-    // try {
-    //   final imageV = ImagePicker();
-    //   final file = await imageV.pickImage(source: ImageSource.gallery);
-    //   firebaseStorage
-    //       .ref('test/test${file?.name}.png')
-    //       .putBlob(await file?.readAsBytes());
-    // } catch (e) {
-    //   final value = e;
-    //   debugPrint('ERROR: $value');
-    // }
-    setState(() {
-      value = text;
-      image =
-          'https://firebasestorage.googleapis.com/v0/b/test-appcheck-ef87e.appspot.com/o/story.png?alt=media&token=0e6fd1f2-f015-4c30-aa6f-228a20db9e78';
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  Future<String> getTextTest() async {
-    final value = await firebaseFirestore.collection('test').get();
-
-    return value.docs.first['test'] as String;
-  }
+class TestWidget extends StatelessWidget {
+  const TestWidget({
+    super.key,
+    required this.logText,
+    required this.isDesk,
+  });
+  final String logText;
+  final bool isDesk;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: ListView(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-                height: 100,
-                width: 100,
-                child: Image(image: NetworkImage(image))),
-            Text(
-              value,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    print(logText);
+    return Container(
+      height: isDesk ? 200 : 100,
+      width: isDesk ? 200 : 100,
+      alignment: Alignment.center,
+      color: Colors.black,
     );
+  }
+}
+
+class ScreenSizeBloc extends Cubit<bool> {
+  final ScreenSizeRepository _screenSizeStream;
+
+  StreamSubscription<Size>? _subscription;
+
+  ScreenSizeBloc()
+      : _screenSizeStream = ScreenSizeRepository(),
+        super(true) {
+    _subscription = _screenSizeStream.sizeStream.listen((size) {
+      emit(size.width > 700);
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
+  }
+}
+
+class ScreenSizeRepository with WidgetsBindingObserver {
+  ScreenSizeRepository() {
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final size = _getScreenSize();
+      _sizeController.add(size);
+    });
+  }
+  final StreamController<Size> _sizeController =
+      StreamController<Size>.broadcast();
+
+  Stream<Size> get sizeStream => _sizeController.stream;
+
+  @override
+  void didChangeMetrics() {
+    final size = _getScreenSize();
+    _sizeController.add(size);
+  }
+
+  Size _getScreenSize() {
+    final platformDispatcher = WidgetsBinding.instance.platformDispatcher;
+    final view = platformDispatcher.views.first;
+    return view.physicalSize / view.devicePixelRatio;
+  }
+
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _sizeController.close();
   }
 }
